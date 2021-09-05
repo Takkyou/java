214a1c3a6c1581a0b8b5f9bda787683598c38f5f
Takkyou <takelucky777@naver.com>
2021-09-05 16:31:32 +0900

    innerclass change

--- innerclasss/InnerTest.java
+++ innerclasss/InnerTest.java
@@ -4,6 +4,27 @@ class OutClass{
 	private int num =10;
 	private static int sNum = 20;
 	
+	
+	static class InstaticClass{
+		int inNum = 100;
+		static int sInNum = 200;
+		
+		void inTest() {
+			System.out.println("InStaticClass inNum = "+ inNum + "(내부 클래스의 인스턴스 변수 사용)");
+			System.out.println("InStaticClass sInNum = "+ sInNum + "내부 클래스의 정적 변수 사용)");
+			System.out.println("OutClass sNum ="+ sNum + "(외부 클래스의정적 변수 사용)");
+		}
+		
+		static void sTest() {
+			System.out.println("OutClass sNum ="+ sNum + "(외부 클래스의 정적 변수 사용)");
+		    System.out.println("InStaticClass sInNum = " + sInNum + "(내부 클래스의 정적 변수 사용)");
+		}
+	}
+	
+	
+	
+	
+	
 	private InClass inClass;
 	
 	public OutClass() {
@@ -26,6 +47,14 @@ class OutClass{
 public class InnerTest {
 
 	public static void main(String[] args) {
+		OutClass.InstaticClass sInClass = new OutClass.InstaticClass();
+		System.out.println("정적 내부 클래스 일반 메서드 호출");
+		sInClass.inTest();
+		System.out.println();
+		System.out.println("정적 내부클래스의 정적 메서드 호출");
+		OutClass.InstaticClass.sTest();
+		sInClass.sTest();
+	
 		OutClass outClass = new OutClass();
 		System.out.println("외부 클래스 이용하여 내부 클래스 기능 호출");
 		outClass.usingClass();
