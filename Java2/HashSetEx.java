8ee07ed5731878da4a0302fa25ebeed53519d62e
Takkyou <takelucky777@naver.com>
2021-10-07 13:44:13 +0900

    hava

---
+++ Java2/HashSetEx.java
@@ -0,0 +1,33 @@
+package Java2;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetEx {
+
+	public static void main(String[] args) {
+		HashSet<Integer> v = new HashSet<Integer>();
+		v.add(5);
+		v.add(4);
+		v.add(-1);
+		v.add(100);
+		v.add(5);
+		
+		Iterator<Integer> it = v.iterator();
+		while(it.hasNext() ) {
+			int n= it.next();
+			System.out.println(n);
+		}
+		
+		it = v.iterator();
+		int sum =0;
+		while(it.hasNext() ) {
+			int n= it.next();
+			sum +=n;
+			System.out.println(sum);
+		}
+	}
+	
+	
+
+}
